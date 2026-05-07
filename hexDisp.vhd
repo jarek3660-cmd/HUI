d5734f0ba346f5b42078473c9db556abd1ab17ae
jarek3660-cmd <jarek3660@gmail.com>
2026-05-07 17:20:21 -0600

    Create hexDisp.vhd

---
+++ hexDisp.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity hexDisp is
+  Port (
+    clk   : in  STD_LOGIC;
+    input : in  STD_LOGIC_VECTOR(3 downto 0);
+    seg   : out STD_LOGIC_VECTOR(7 downto 0)
+  );
+end hexDisp;
+
+architecture Behavioral of hexDisp is
+
+  component ROM1
+    port (
+      a    : in  STD_LOGIC_VECTOR(3 downto 0);
+      clk  : in  STD_LOGIC;
+      qspo : out STD_LOGIC_VECTOR(7 downto 0)
+    );
+  end component;
+
+begin
+
+  sevenSegValues : ROM1
+    port map (
+      a    => input,
+      clk  => clk,
+      qspo => seg
+    );
+
+end Behavioral;
